2ff512300dc1266fb08074657edc7d11d9d1a6d9
Lucjan Zenczuk <lucjan.zenczuk@gmail.com>
2017-07-12 10:46:04 +0100

    MVP-1 3 and 4 - WS endpoint and connecting web app to it
    There is e problem with closing connection using command. Not it doesn't work.

--- src/main/java/com/github/lzenczuk/rxwsgame/Main.java
+++ src/main/java/com/github/lzenczuk/rxwsgame/Main.java
@@ -2,11 +2,18 @@ package com.github.lzenczuk.rxwsgame;
 
 import com.github.davidmoten.rx.Bytes;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 import io.netty.handler.logging.LogLevel;
 import io.reactivex.netty.protocol.http.server.HttpServer;
+import rx.Observable;
+import rx.subjects.PublishSubject;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by dev on 10/07/17.
@@ -24,7 +31,74 @@ public class Main {
                 System.out.println("WS uri: "+uri);
                 if(request.isWebSocketUpgradeRequested()){
                     System.out.println("WS");
-                    return response.setStatus(HttpResponseStatus.INTERNAL_SERVER_ERROR);
+
+                    return response.acceptWebSocketUpgrade(wsConnection -> {
+
+                        PublishSubject<Void> closeConnection = PublishSubject.create();
+
+                        /*wsConnection.writeAndFlushOnEach(outputPublisher.map(s -> {
+                            System.out.println("Output write: "+s);
+                            return new BinaryWebSocketFrame(Unpooled.copiedBuffer(s.getBytes()));
+                        }));*/
+
+                        /*String[] msges = new String[3];
+                        msges[0]="test1";
+                        msges[1]="test2";
+                        msges[2]="test3";*/
+
+                        System.out.println("----------> create output observable");
+                        Observable<WebSocketFrame> outOb = Observable.interval(1l, TimeUnit.SECONDS)
+                                .map(aLong -> {
+                                    System.out.println("Tick mapp");
+                                    return "Tick: " + aLong;
+                                })
+                                .onBackpressureBuffer()
+                                .map(s -> {
+                                    System.out.println("Output write: " + s);
+                                    //return new TextWebSocketFrame(s);
+                                    return new BinaryWebSocketFrame(Unpooled.copiedBuffer(s.getBytes()));
+                                }).cast(WebSocketFrame.class);
+
+                        /*Observable<WebSocketFrame> outOb = Observable.from(msges).map(s -> {
+                            System.out.println("Output write: " + s);
+                            return new BinaryWebSocketFrame(Unpooled.copiedBuffer(s.getBytes()));
+                        });*/
+
+                        wsConnection.getInput()
+                                .filter(wsf -> {
+                                    if(wsf instanceof BinaryWebSocketFrame){
+                                        return true;
+                                    }else{
+                                        wsf.release();
+                                        return false;
+                                    }
+                                })
+                                .cast(BinaryWebSocketFrame.class)
+                                .forEach(bwsf -> {
+                                    byte type = bwsf.content().getByte(0);
+                                    switch (type){
+                                        case 0:
+                                            System.out.println("Zero");
+                                            break;
+                                        case 1:
+                                            System.out.println("One");
+                                            break;
+                                        case 127:
+                                            System.out.println("#################################");
+                                            System.out.println("-------> End");
+                                            System.out.println("#################################");
+                                            closeConnection.onCompleted();
+                                            break;
+                                        default:
+                                            System.out.println("Unknown");
+                                    }
+
+                                    bwsf.release();
+                                });
+
+                        System.out.println("----------> Write output");
+                        return Observable.merge(closeConnection, wsConnection.writeAndFlushOnEach(outOb));
+                    });
                 }else{
                     System.out.println("Not WS");
                     return response.setStatus(HttpResponseStatus.INTERNAL_SERVER_ERROR);
--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -4,4 +4,40 @@
 
 $(document).ready(function () {
     $("#msg").text("Hello js")
+
+    var ws = new WebSocket("ws://localhost:8088/ws");
+
+    ws.onerror = function (ev) {
+        console.log("Error: "+ev)
+    };
+
+    ws.onmessage = function (ev) {
+        console.log("Message: "+JSON.stringify(ev))
+    };
+
+    ws.onclose =  function (ev) {
+        console.log("Close: "+JSON.stringify(ev))
+    };
+
+    ws.onopen = function (ev) {
+        console.log("Open: "+JSON.stringify(ev));
+
+        var bytearray = new Uint8Array(1);
+        bytearray[0] = 0;
+        ws.send(bytearray.buffer);
+
+        bytearray[0] = 1;
+        ws.send(bytearray.buffer);
+
+        bytearray[0] = 2;
+        ws.send(bytearray.buffer);
+
+        window.setTimeout(function () {
+            bytearray[0] = 127;
+            ws.send(bytearray.buffer);
+        }, 10000);
+
+
+        console.log("-----> Done")
+    };
 });
